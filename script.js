6c96e04b8772fa120f1d3f6dd2f972ca660b0ffe
Caglar <2038887+ycaglar@users.noreply.github.com>
2022-03-20 22:06:07 -0500

    Enable webcam on page load

--- script.js
+++ script.js
@@ -9,48 +9,7 @@ function getUserMediaSupported() {
 // If webcam supported, add event listener to button for when user
 // wants to activate it to call enableCam function which we will
 // define in the next step.
-function enableCamera() {
-  if (getUserMediaSupported()) {
-    if (!model) {
-      return;
-    }
-    const constraints = {
-      video: true
-    };
-    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-      video.srcObject = stream;
-      video.addEventListener('loadeddata', predictWebcam);
-    });
-    // enableWebcamButton.addEventListener('click', enableCam);
-  } else {
-    console.warn('getUserMedia() is not supported by your browser');
-  }
-};
-window.onload = enableCamera();
-if (getUserMediaSupported()) {
-  if (!model) {
-    return;
-  }
-  const constraints = {
-    video: true
-  };
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-    video.srcObject = stream;
-    video.addEventListener('loadeddata', predictWebcam);
-  });
-  // enableWebcamButton.addEventListener('click', enableCam);
-} else {
-  console.warn('getUserMedia() is not supported by your browser');
-}
-// Enable the live webcam view and start classification.
-function enableCam(event) {
-  // Only continue if the COCO-SSD has finished loading.
-  if (!model) {
-    return;
-  }
-  // Hide the button once clicked.
-  event.target.classList.add('removed');
-  // getUsermedia parameters to force video but not audio.
+if (getUserMediaSupported() && !model) {
   const constraints = {
     video: true
   };
@@ -60,6 +19,32 @@ function enableCam(event) {
     video.addEventListener('loadeddata', predictWebcam);
   });
 }
+// if (getUserMediaSupported()) {
+//   //window.addEventListener('load', enableCam);
+//   //enableWebcamButton.addEventListener('click', enableCam);
+//   enableCam();
+// } else {
+//   console.warn('getUserMedia() is not supported by your browser');
+// }
+// Enable the live webcam view and start classification.
+// function enableCam() {
+//   // Only continue if the COCO-SSD has finished loading.
+//   if (!model) {
+//     return;
+//   }
+//   // Hide the button once clicked.
+//   event.target.classList.add('removed');
+//   // getUsermedia parameters to force video but not audio.
+//   const constraints = {
+//     video: true
+//   };
+//   // Activate the webcam stream.
+//   navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+//     window.alert('navigator.mediaDevices.getUserMedia(constraints).then(function(stream)');
+//     video.srcObject = stream;
+//     video.addEventListener('loadeddata', predictWebcam);
+//   });
+// }
 // Store the resulting model in the global scope of our app.
 var model = undefined;
 // Before we can use COCO-SSD class we must wait for it to finish
